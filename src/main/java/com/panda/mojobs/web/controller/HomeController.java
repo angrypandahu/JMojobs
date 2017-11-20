027403906d48112990cd882964b80949241b5c35
hupanpan <hupanpan@nipponpaint.com.cn>
2017-11-20 09:43:46 +0800

    提交

--- src/main/java/com/panda/mojobs/web/controller/HomeController.java
+++ src/main/java/com/panda/mojobs/web/controller/HomeController.java
@@ -14,38 +14,15 @@ public class HomeController {
 
     @RequestMapping(value = "", method = {RequestMethod.GET})
     public String home(HttpServletRequest request) {
-        String method = request.getMethod();
-        System.out.println("request--->" + method);
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        for (String s : parameterMap.keySet()) {
-            String[] strings = parameterMap.get(s);
-            for (String oneValue : strings) {
-                System.out.println("key:" + s + " -->  " + oneValue);
-            }
-        }
+
+
         return "home/home";
     }
 
-    @RequestMapping(value = "", method = {RequestMethod.POST})
-    @ResponseBody
-    public String homePost(HttpServletRequest request) {
-        String method = request.getMethod();
+    @RequestMapping(value = "", method = {RequestMethod.GET})
+    public String search(HttpServletRequest request) {
 
-        System.out.println("request--->" + method);
-        Map<String, String[]> parameterMap = request.getParameterMap();
-
-        for (String s : parameterMap.keySet()) {
-            String[] strings = parameterMap.get(s);
-            for (String oneValue : strings) {
-                System.out.println("key:" + s + " -->  " + oneValue);
-            }
-        }
-        return "success";
-    }
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(Model model) {
-        model.addAttribute("name", "Dear");
-        return "hello";
+        return "home/home";
     }
 
 }
