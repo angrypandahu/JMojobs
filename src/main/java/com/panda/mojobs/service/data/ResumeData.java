ff2efb1fdad7c14b1f0d6d5ecda78642d53eb227
hupanpan <hupanpan@nipponpaint.com.cn>
2017-11-27 14:32:18 +0800

    合并backend2

---
+++ src/main/java/com/panda/mojobs/service/data/ResumeData.java
@@ -0,0 +1,40 @@
+package com.panda.mojobs.service.data;
+
+import com.panda.mojobs.service.dto.BasicInformationDTO;
+import com.panda.mojobs.service.dto.ExperienceDTO;
+import com.panda.mojobs.service.dto.ResumeDTO;
+
+import java.util.List;
+
+/**
+ * Created by hupanpan on  2017/11/22.
+ */
+public class ResumeData {
+    private ResumeDTO resumeDTO;
+    private BasicInformationDTO basicInformationDTO;
+    private List<ExperienceDTO> experienceDTOList;
+
+    public ResumeDTO getResumeDTO() {
+        return resumeDTO;
+    }
+
+    public void setResumeDTO(ResumeDTO resumeDTO) {
+        this.resumeDTO = resumeDTO;
+    }
+
+    public BasicInformationDTO getBasicInformationDTO() {
+        return basicInformationDTO;
+    }
+
+    public void setBasicInformationDTO(BasicInformationDTO basicInformationDTO) {
+        this.basicInformationDTO = basicInformationDTO;
+    }
+
+    public List<ExperienceDTO> getExperienceDTOList() {
+        return experienceDTOList;
+    }
+
+    public void setExperienceDTOList(List<ExperienceDTO> experienceDTOList) {
+        this.experienceDTOList = experienceDTOList;
+    }
+}
